3a80e6697c46eb3d06ebaad0cd9a4c6050d98be3
makise <10175101106@stu.ecnu.edu.cn>
2022-05-30 08:57:37 +0800

    initial commit

---
+++ src/main/java/edu/ecnu/journalmanage/JournalManageApplication.java
@@ -0,0 +1,13 @@
+package edu.ecnu.journalmanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JournalManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JournalManageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/ecnu/journalmanage/JournalManageApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ecnu.journalmanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournalManageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
