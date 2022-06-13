d4dbf155a5d62e02d6d7e4a960e8609777fc9a2c
makise <10175101106@stu.ecnu.edu.cn>
2022-06-14 00:48:52 +0800

    fix: add ArticleService to provide select article by id

---
+++ src/main/java/edu/ecnu/journalmanage/service/ArticleService.java
@@ -0,0 +1,18 @@
+package edu.ecnu.journalmanage.service;
+
+import edu.ecnu.journalmanage.mapper.ArticleMapper;
+import edu.ecnu.journalmanage.model.Article;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ArticleService {
+    final ArticleMapper articleMapper;
+
+    public ArticleService(ArticleMapper articleMapper) {
+        this.articleMapper = articleMapper;
+    }
+
+    Article getArticleById(int articleId) {
+        return articleMapper.getArticleById(articleId);
+    }
+}
